460387485c731c5e17709cccc48be5a1e58bceca
Neil Jenkins <neil@nmjenkins.com>
2010-11-25 22:42:32 +0100

    New preferences and auto-translate.
    The extension can now always translate or never translate on a language by
    language basis. The preference for each language can be set in the new
    preferences page or whenever you encounter a page in that language.
    
    The native language to translate into is now chosen in the first instance by
    detecting the browser locale; this can be changed in the preferences as well.

---
+++ options.js
@@ -0,0 +1,117 @@
+var attributeNameMap = {
+    'className': 'className',
+    'for': 'htmlFor',
+    html: 'innerHTML',
+    text: 'textContent'
+};
+
+var Element = {
+
+    create: function ( tag, props, children ) {
+        var el = document.createElement( tag );
+        if ( props ) {
+            Element.setAttributes( el, props );
+        }
+        if ( children ) {
+            for ( var i = 0, l = children.length; i < l; i += 1 ) {
+                el.appendChild( children[i] );
+            }
+        }
+        return el;
+    },
+
+    setAttributes: function ( el, props ) {
+        for ( var prop in props ) {
+            if ( props.hasOwnProperty( prop ) ) {
+                var name = attributeNameMap[ prop ],
+                    value = props[ prop ];
+                if ( value ) {
+                    if ( name ) {
+                        el[ name ] = value;
+                    } else {
+                        el.setAttribute( prop, value );
+                    }
+                }
+            }
+        }
+    }
+};
+
+window.addEventListener( 'DOMContentLoaded', function () {
+
+    var storage = localStorage;
+
+    function optionChanged( e ) {
+        var element = e.currentTarget;
+        var name = element.name;
+        var value = element.value;
+
+        // Set value
+        storage.setItem( name, value );
+    }
+
+    var optList = document.getElementById( 'optionsList' );
+    var homeLangSelect = document.getElementById( 'homeLang' ),
+        browserLang = window.navigator.language,
+        homeLang = storage.getItem( 'homeLang' ) ||
+            ( Translate.languages[ browserLang ] ? browserLang : 'en' );
+
+    // 1. Create the options
+    for ( var lang in Translate.languages ) {
+
+        var pref = storage.getItem( lang ) || 'ask',
+            language = Translate.languages[ lang ];
+
+        homeLangSelect.appendChild( Element.create( 'option', {
+            text: language,
+            value: lang,
+            selected: homeLang === lang ? 'selected' : null
+        }) );
+
+        var optionsSelect, langSettings = Element.create( 'fieldset', null, [
+            Element.create( 'label', {
+                text: language,
+                className: 'optionlabel'
+            }),
+            optionsSelect = Element.create( 'select', {
+                name: lang
+            }, [
+                Element.create( 'option', {
+                    text: 'Always translate ' + language,
+                    value: 'always',
+                    selected: pref === 'always' ? 'selected' : null
+                }), Element.create( 'option', {
+                    text: 'Ask before translating ' + language,
+                    value: 'ask',
+                    selected: pref === 'ask' ? 'selected' : null
+                }), Element.create( 'option', {
+                    text: 'Never translate ' + language,
+                    value: 'never',
+                    selected: pref === 'never' ? 'selected' : null
+                })
+            ])
+        ]);
+
+        // listen for changes
+        optionsSelect.addEventListener( 'change', optionChanged, true );
+
+        optList.appendChild( langSettings );
+    }
+    homeLangSelect.addEventListener( 'change', function() {
+        opera.extension.postMessage({
+            action: 'setHomeLang',
+            data: homeLangSelect.value
+        });
+    }, false );
+    document.getElementById( 'clearPreferences' ).addEventListener(
+            'click', function () {
+        opera.extension.postMessage({
+            action: 'resetPreferences'
+        });
+    }, false );
+    opera.extension.addEventListener( 'message', function ( msg ) {
+        if ( msg.data.action === 'preferencesReset' ) {
+            window.location.reload();
+        }
+    }, false );
+}, false );
